f57456081361dd180f76ff8eae5746eb79f91253
Sam Kach <samwelmoyi@gmail.com>
2020-05-06 10:11:43 +0300

    winning the game

--- src/Board.js
+++ src/Board.js
@@ -83,7 +83,7 @@ class Board extends Component {
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
-    let hasWon = false;
+    let hasWon = board.every((row) => row.every((cell) => !cell));
 
     this.setState({ board, hasWon });
   }
@@ -93,6 +93,9 @@ class Board extends Component {
   render() {
     // if the game is won, just show a winning msg & render nothing else
     // TODO
+    if (this.state.hasWon) {
+      return <h1>YOU WON!!</h1>;
+    }
     // make table board
     // TODO
     let tblBoard = [];
