16cb4ececdce97a9e1538f3b64b7281ed0298aa7
Sam Kach <samwelmoyi@gmail.com>
2020-05-06 11:06:29 +0300

    styling board

--- src/Board.js
+++ src/Board.js
@@ -94,7 +94,14 @@ class Board extends Component {
     // if the game is won, just show a winning msg & render nothing else
     // TODO
     if (this.state.hasWon) {
-      return <h1>YOU WON!!</h1>;
+      return (
+        <div className="Board-title">
+          <div className="winner">
+            <span className="neon-orange">YOU</span>
+            <span className="neon-blue">WIN!</span>
+          </div>
+        </div>
+      );
     }
     // make table board
     // TODO
@@ -114,9 +121,15 @@ class Board extends Component {
       tblBoard.push(<tr key={y}>{row}</tr>);
     }
     return (
-      <table className="Board">
-        <tbody>{tblBoard}</tbody>
-      </table>
+      <div>
+        <div className="Board-title">
+          <div className="neon-orange">Lights</div>
+          <div className="neon-blue">Out</div>
+        </div>
+        <table className="Board">
+          <tbody>{tblBoard}</tbody>
+        </table>
+      </div>
     );
   }
 }
